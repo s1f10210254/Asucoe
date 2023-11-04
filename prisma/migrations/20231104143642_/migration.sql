8b4e9b1b75d15830fcca5136cfb2da2d8e38e58c
s1f10210254 <s1f10210248@iniad.org>
2023-11-05 00:18:16 +0900

    deploy用

--- prisma/migrations/20231104143642_/migration.sql
+++ prisma/migrations/20231104143642_/migration.sql
@@ -18,6 +18,17 @@ CREATE TABLE "Calendar" (
     CONSTRAINT "Calendar_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL DEFAULT '',
+    "gender" TEXT NOT NULL DEFAULT '',
+    "age" TEXT NOT NULL DEFAULT '',
+    "profession" TEXT NOT NULL DEFAULT '',
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "Message_calendarId_key" ON "Message"("calendarId");
 
