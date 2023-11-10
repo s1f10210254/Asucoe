759ab7949ebf019a5e3a9b911069340cf3a04a60
s1f10210254 <s1f10210248@iniad.org>
2023-11-11 02:06:11 +0900

    login機能実装deploy画面で

--- pages/develop.tsx
+++ pages/develop.tsx
@@ -1,6 +1,7 @@
 import { ShowModel } from "@/component/Home/ShowModel/ShowModel";
 import { Loading } from "@/component/Loading/Loading";
 import { useEffect, useState } from "react";
+import { createClient } from '@supabase/supabase-js'
 
 export default function Develop(){
     const [isLoading, setIsLoading] = useState(true)
@@ -14,8 +15,27 @@ export default function Develop(){
         }
     },[])
 
+    const supabaseUrl = process.env.SUPABASEURL
+    const supabaseAnonKey = process.env.SUPABASEANONKEY
+    if(supabaseUrl === undefined) return;
+    if(supabaseAnonKey === undefined) return;
+    
+    const supabase = createClient(supabaseUrl, supabaseAnonKey);
+    const handleLogin = async()=>{
+        const {data, error} = await supabase.auth.signInWithOAuth({
+            provider: 'github',
+        })
+        console.log("login")
+    }
+
+    const handleLogout=async()=>{
+        const {error} = await supabase.auth.signOut()
+        console.log("logout")
+    }
+
+
     return (
-        <div>
+        <div style={{display: "flex"}}>
             {/* {isLoading ? (
                 <div>
                     <Loading/>
@@ -25,7 +45,13 @@ export default function Develop(){
                     <ShowModel/>
                 </div>
             )} */}
-            <p>aa</p>
+            <div >
+            <button onClick={handleLogin}>Ligin with GitHub</button>
+            </div>
+            <div>
+            <button onClick={handleLogout}>Logout</button>
+
+            </div>
         </div>
     )
 }
